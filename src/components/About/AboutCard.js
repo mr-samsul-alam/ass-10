13ae54fe3f317007cbaeec20c17abd02824612d9
mr-samsul-alam <contactsamsulalam@gmail.com>
2021-10-18 03:39:03 +0600

    commit-time-3.38Am

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+
+const AboutCard = (props) => {
+    const {_id, image, name, subject, degree } = props.doctor
+    return (
+        <div>
+            <Col>
+                <Card className="w-75">
+                    <Card.Img variant="top" src={image}  className="w-50 mx-auto" />
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>{degree}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </div>
+    );
+};
+
+export default AboutCard;
